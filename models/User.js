247ae410579901640961999009ad6c2798e90fd1
unknown <atix.algo@gmail.com>
2025-10-01 04:44:41 +0600

    completed

--- models/User.js
+++ models/User.js
@@ -4,7 +4,10 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  role: { type: String, default: 'customer' }
+  role: { type: String, default: 'customer' },
+  photoURL: { type: String }, 
+  experience: { type: String, default: 'N/A' },
+  specialties: { type: [String], default: [] } 
 });
 
 const User = mongoose.model('User', userSchema);
